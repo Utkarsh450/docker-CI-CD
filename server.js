09a3bb8f94c5af6b66e1c3aa30258e917b160daa
Utkarsh450 <ubarnwal0802@gmail.com>
2026-02-27 19:07:05 +0530

    Merge8

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server is running on port which is my given ${PORT}`);
+    console.log(`Server is currently running on port which is my given ${PORT}`);
 });
